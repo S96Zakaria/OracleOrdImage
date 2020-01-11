0026c6ccb718c2fcc3c2a8ee4adeb89fda326db7
BlackAngel <souhailxelfahimi@gmail.com>
2020-01-12 00:45:13 +0100

    frontEnd

--- src/main/java/com/imagengine/demo/ImagengineApplication.java
+++ src/main/java/com/imagengine/demo/ImagengineApplication.java
@@ -3,25 +3,29 @@ package com.imagengine.demo;
 import com.imagengine.demo.service.ImageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class ImagengineApplication  {
+public class ImagengineApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ImagengineApplication.class, args);
-        /*ImageService imageService = new ImageService();
-        File file = new File(System.getProperty("user.dir")+"/src/main/resources/static/images/image.jpg");
-        imageService.updateAndInsertImage(BigDecimal.valueOf(3),file);*/
-        //System.out.println("hjkgskjsh");
 
     }
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Register resource handler for images
+        registry.addResourceHandler("/images/**").addResourceLocations("/static/images/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
+
 }
