26d2bfdd21148dc7ee2270f3b5e1baefb588ef10
BlackAngel <souhailxelfahimi@gmail.com>
2020-01-12 22:49:42 +0100

    'compareclass'

---
+++ src/main/java/com/imagengine/demo/bean/Compare.java
@@ -0,0 +1,29 @@
+package com.imagengine.demo.bean;
+
+import java.math.BigDecimal;
+
+public class Compare {
+
+	private  BigDecimal id1;
+	private  BigDecimal id2;
+	private  float score;
+	public BigDecimal getId1() {
+		return id1;
+	}
+	public void setId1(BigDecimal id1) {
+		this.id1 = id1;
+	}
+	public BigDecimal getId2() {
+		return id2;
+	}
+	public void setId2(BigDecimal id2) {
+		this.id2 = id2;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+
+}
